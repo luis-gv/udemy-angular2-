d7963dd386b6f030044d94369a41691b451d4036
Jose Luis GV <luis_gv2@yahoo.com.mx>
2017-07-03 23:49:29 -0500

    Pipes currency terminado

--- udemy/pipes/src/app/app.component.ts
+++ udemy/pipes/src/app/app.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre:string = "Jose Luis";
+  arreglo = [10,20,30,40,50,60,70,80,90,100];
   title = 'app works!';
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.45;
 }
