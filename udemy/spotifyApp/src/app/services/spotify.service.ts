8d09b8e6cb21a9bdaf6295a493bdde542074f521
Jose Luis GV <luis_gv2@yahoo.com.mx>
2017-07-12 23:41:09 -0500

    Se agrega routes y primeras vistas para spotify project

---
+++ udemy/spotifyApp/src/app/services/spotify.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import{ Http } from '@angular/http'
+import{ } from 'rxjs/add/operator/map'
+
+@Injectable()
+export class SpotifyService {
+  artistas:any = [];
+  urlBusqueda:string = "https://api.spotify.com/v1/search";
+
+  constructor(private http:Http) { }
+
+  getArtistas(termino:string){
+    let query = `q=${termino}&type=artist`;
+    let url = this.urlBusqueda + query;
+
+    return this.http.get(url)
+            .map(res =>{
+              console.log(res)
+            });
+  }
+
+}
